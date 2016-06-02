097eb6d886651b0faee5694e9d0298623cef6a56
Filipe Spindola <filipe.spindola@hotmail.com>
2016-06-01 21:17:31 -0300

    Section 5: Understanding AngularJS Scope

--- section4/lesson1/main.js
+++ section4/lesson1/main.js
@@ -2,6 +2,10 @@ var app = angular.module('codecraft', []);
 
 app.controller('ParentController', function ($scope) {
 	$scope.name = "Parent";
+    
+    $scope.reset = function () {
+        $scope.name = "Parent";
+    };
 
 	$scope.reset = function () {
 		$scope.name = "Parent";
